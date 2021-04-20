0250a28816f193222c4436e41f024ea6c498af6d
Marek Sabo <marek.sabo@firma.seznam.cz>
2021-04-20 21:28:49 +0200

    Add ingredients textarea with markdown

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_161323) do
+ActiveRecord::Schema.define(version: 2021_04_20_185438) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.text "instructions"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "ingredients"
   end
 
 end
